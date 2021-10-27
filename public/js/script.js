83dc9307eb9ff7b0172749b9fd08035d0392a7c1
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-10-27 15:53:33 -0400

    Reverted to a working state

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,4 @@
-require("dotenv").config();
-
-const apiKey = process.env.TM_APIKEY;
+const apiKey = "MQQSELn5pJ4IejXfT0t5DgufSAGg3gZt";
 const tmUrl = "https://app.ticketmaster.com/discovery/v2/";
 
 
