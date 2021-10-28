6db5c516f1d4f0107ba70f29f96a42794d38d8f9
Eric Rion <erion42@gmail.com>
2021-10-27 22:17:34 -0400

    Database and tables added in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -7,14 +7,22 @@ CREATE DATABASE events_db;
 USE events_db;
 
 
--- -- Create Table 1 - Users
+-- Create Table 1 - Users
+ DROP TABLE IF EXISTS User;
  CREATE TABLE User (
-     id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    userID VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    userPassword VARCHAR(50) NOT NULL
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
  );
 
-CREATE TABLE Attraction (
-    eventID VARCHAR(50) NOT NULL,
-);
+-- Create Table 2 - Events
+ DROP TABLE IF EXISTS Events;
+ CREATE TABLE Events (
+     eventID VARCHAR(50) NOT NULL,
+     eventName VARCHAR(255),
+     geoLat INT(50) NOT NULL,
+     geoLon INT(50) NOT NULL,
+     eventDate VARCHAR(50) NOT NULL,
+     eventDescription VARCHAR(255)
+ ); 
