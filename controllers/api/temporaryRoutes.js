a4dc2306ab77dec7052862ddff15c8ca913ccfab
Eric Rion <erion42@gmail.com>
2021-10-29 19:43:10 -0400

    Modeled after eventRoutes.js

--- controllers/api/temporaryRoutes.js
+++ controllers/api/temporaryRoutes.js
@@ -5,30 +5,25 @@ const { Temporary } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
-        console.log("This one?", req.body);
-        const newTemp = await Temporary.create({
+        const event = await ({
             //id, tempEmail, tempPassword, geoLat, geoLon
-            tempEmail: req.body.email,
-            tempPassword: req.body.password,
+            // tempEmail: req.body.email,
+            // tempPassword: req.body.password,
             // geoLat: req.body.DERP,
             // geoLon: req.body.DERP,
         });
-        console.log("Temporary Data", newTemp);
-        if (newTemp) {
-            await req.session.save(() => {
-                req.session.tempEmail = newTemp.email;
-                req.session.tempPassword = newTemp.password;
-                // req.session.geoLat = newTemp.DERP;
-                // req.session.geoLon = newTemp.DERP;
-                req.session.loggedIn = true;
+        
+        if (!tempEvent) {
+            const tempEvent = tempEvent.create({
 
-                res.json(newTemp);
-            })
-        };
+            });
+
+            res.json(tempEvent);
+        } else {
+
+        }
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
- 
-//  module.exports = router;
