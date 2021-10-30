b04d6b9f66be345c451ae20c2ccf5e83a26aba42
James Strande <strandejr@gmail.com>
2021-10-30 16:28:40 -0400

    connected events and tempEvents to user through models index

--- models/index.js
+++ models/index.js
@@ -2,4 +2,18 @@ const User = require('./User');
 const Event = require('./Event');
 const Temp = require('./Temporary');
 
+User.hasMany(Event, {
+    foreignKey: 'eventId',
+});
+
+Event.belongsToMany(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+Temp.belongsToMany(User, {
+    foreignKey: 'userId',
+});
+
+
 module.exports = { User, Event, Temp };
